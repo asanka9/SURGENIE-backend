d66545a7c94281b72e93428780c3205355d29b60
asanka9 <sendasanka96@gmail.com>
2022-02-12 07:11:19 +0530

    update

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from user import views
 
 urlpatterns = [
-
     path('profile/', views.ProfileView.as_view()),
     path('api-auth/', views.CustomAuthToken.as_view()),
     path('register/', views.registerUser),
@@ -12,8 +11,6 @@ urlpatterns = [
     path('user-info/', views.userInfo),
     path('verify-token/', views.verify_token),
     path('logout/', views.verify_token),
-
-
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
